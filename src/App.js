13fc87a43395812a844b8d52821b13c19b4ad803
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-10-24 18:40:11 -0300

    [REQ-07] configure react router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      {/* <Route exact path="/" component={ } /> */}
+      {/* <Route exact path="/meals" component={} /> */}
+      {/* <Route exact path="/drinks" component={ } /> */}
+      {/* <Route exact path="/meals/:id-da-receita" component={ } /> */}
+      {/* <Route exact path="/drinks/:id-da-receita" component={ } /> */}
+      {/* <Route exact path="/meals/:id-da-receita/in-progress" component={ } /> */}
+      {/* <Route exact path="/profile" component={ } /> */}
+      {/* <Route exact path="/done-recipes" component={ } /> */}
+      {/* <Route exact path="/favorite-recipes" component={ } /> */}
+    </BrowserRouter>
   );
 }
 
