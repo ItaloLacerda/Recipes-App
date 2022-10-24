c1653bfc0942a6b0b4b250a0b7245b962fc2d29e
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-10-24 19:10:33 -0300

    [REQ-07] Implement component header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <>
+      <h3 data-testid="page-title">Titulo da pagina</h3>
+      <div>
+        <object
+          data-testid="profile-top-btn"
+          type="image/svg+xml"
+          data={ profileIcon }
+        >
+          profileIcon
+        </object>
+      </div>
+      <div>
+        <object
+          data-testid="search-top-btn"
+          type="image/svg+xml"
+          data={ searchIcon }
+        >
+          searchIcon
+        </object>
+      </div>
+    </>
   );
 }
