d33cc698911c52f82a5400a97036dc6a431877bd
Ayanara <aya.nathane@gmail.com>
2022-10-25 15:57:09 -0300

    [REQ-02 e 03] - cria os elementos da tela de login, os inputs e button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,47 @@
 import React from 'react';
 
-export default function Login() {
-  return (
-    <div>Login</div>
-  );
+class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = (event) => {
+    const { target: { name, value } } = event;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <form>
+        <input
+          type="email"
+          data-testid="email-input"
+          name="email"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          value={ password }
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="login-submit-btn"
+          className="BTN_login"
+          type="submit"
+        >
+          Entrar
+        </button>
+      </form>
+    );
+  }
 }
+
+export default Login;
