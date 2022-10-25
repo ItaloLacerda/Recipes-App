6b3fee6b19707682fd2c0a36ed79173c1fa6d326
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-10-25 14:31:36 -0300

    [REQ-09] write test if clicking on the Profile icon redirects to the path /profile

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import Header from '../components/Header';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
@@ -26,6 +26,28 @@ describe('Testa Componete Header', () => {
       expect(titlePage).toBeInTheDocument();
       expect(profileTopBtn).toBeInTheDocument();
       expect(searchTopBtn).toBeInTheDocument();
+    }, 3000);
+  });
+
+  test('Testa se ao clicar no icone de Perfil redireciona para o caminho "/profile"', async () => {
+    const { history } = renderWithRouterAndRedux(<App />, { initialEntries: ['/meals'] });
+
+    expect(history.location.pathname).toBe('/meals');
+
+    setTimeout(async () => {
+      let titlePage = screen.getByRole('heading', {
+        name: /meals/i,
+      });
+      const profileTopBtn = screen.getByTestId('profile-top-btn');
+      expect(titlePage).toBeInTheDocument();
+      expect(profileTopBtn).toBeInTheDocument();
+
+      userEvent.click(profileTopBtn);
+      titlePage = screen.getByRole('heading', {
+        name: /profi/i,
+      });
+      console.log(titlePage);
+      expect(titlePage).toBeInTheDocument();
     }, 3000);
   });
 });
