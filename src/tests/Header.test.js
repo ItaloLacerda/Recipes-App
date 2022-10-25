f97b69bd9cbaa6ae222b8efc17bac355da261aa6
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-10-25 11:56:41 -0300

    [REQ-08] performs tests with 45% coverage of the Header component

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
 import App from '../App';
+import Header from '../components/Header';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 
 describe('Testa Componete Header', () => {
-  test('', async () => {
+  test('Testa se o componente Header rendeiriza o titulo', () => {
+    renderWithRouterAndRedux(<Header pageTitle="Test" renderProfileIcon renderSearchIcon />);
+
+    const titlePage = screen.getByTestId('page-title');
+
+    expect(titlePage).toBeInTheDocument();
+  });
+  test('Testa se o componente Header existe na rota "/done-recipes"', async () => {
     const { history } = renderWithRouterAndRedux(<App />, { initialEntries: ['/done-recipes'] });
 
     expect(history.location.pathname).toBe('/done-recipes');
 
-    await waitFor(() => {
+    setTimeout(() => {
       const titlePage = screen.getByTestId('page-title');
       const profileTopBtn = screen.getByTestId('profile-top-btn');
       const searchTopBtn = screen.getByTestId('search-top-btn');
@@ -17,6 +26,6 @@ describe('Testa Componete Header', () => {
       expect(titlePage).toBeInTheDocument();
       expect(profileTopBtn).toBeInTheDocument();
       expect(searchTopBtn).toBeInTheDocument();
-    });
+    }, 3000);
   });
 });
