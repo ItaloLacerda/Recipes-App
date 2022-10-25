26c09c3e2df6a1b262b8669d25a29b687bfb6136
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-10-24 21:56:01 -0300

    [REQ-07] implement header in Recipes component

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import Header from '../components/Header';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default function Recipes() {
+import Header from '../components/Header';
+import { renderHeader } from '../redux/actions';
+
+function Recipes({ updateHeader }) {
+  useEffect(() => {
+    updateHeader('Done Recipes', true, false);
+  }, []);
   return (
     <Header />
   );
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  updateHeader: (pageTitle, profileIcon, searchIcon) => (
+    dispatch(renderHeader(pageTitle, profileIcon, searchIcon))),
+});
+
+Recipes.propTypes = {
+  updateHeader: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Recipes);
