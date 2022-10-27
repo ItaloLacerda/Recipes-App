adb59e51817bebdc4180a69784cb0e63186e214f
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-10-27 18:17:51 -0300

    [REQ-22] Implements the filter as a toggle, which if selected again, the app should return recipes without any filter

--- src/components/TagsForFilter.js
+++ src/components/TagsForFilter.js
@@ -7,22 +7,38 @@ import { rendersRecipe } from '../redux/actions';
 function TagsForFilters({ history, setRecipes }) {
   const cinco = 5;
   const LENGTH = 11;
+  const type = history.location.pathname.substring(1);
+  const filterRecipesSelect = {
+    Beef: false,
+    Breakfast: false,
+    Chicken: false,
+    Dessert: false,
+    Goat: false,
+    Ordinary_Drink: false,
+    Cocktail: false,
+    Shake: false,
+    Other_Unknown: false,
+    Cocoa: false,
+  };
   const [fetchCategory, setFetchCategory] = useState([]);
   const [buttonAll, setButtonAll] = useState(false);
-  const type = history.location.pathname.substring(1);
+  const [filterSelect, setfilterSelect] = useState(filterRecipesSelect);
 
   const fetch = async () => {
     const response = await fetchByCategory(type);
+    console.log(response);
     setFetchCategory(response);
   };
 
   const handelClick = async ({ target }) => {
-    if (target.name === 'buttonAll') {
+    if (target.name === 'buttonAll' || filterSelect[target.name]
+    || filterSelect.Ordinary_Drink || filterSelect.Other_Unknown) {
       const fetchData = await fetchMealsAndDrinks(history.location.pathname);
 
       const data = fetchData.filter((_, index) => index <= LENGTH);
       setRecipes(data);
       setButtonAll(false);
+      setfilterSelect(filterRecipesSelect);
     } else {
       const recipesFilter = await filterByCategory(
         target.name,
@@ -31,6 +47,27 @@ function TagsForFilters({ history, setRecipes }) {
       const data = recipesFilter.filter((_, index) => index <= LENGTH);
       setButtonAll(true);
       setRecipes(data);
+
+      switch (target.name) {
+      case 'Ordinary Drink':
+        setfilterSelect({
+          ...filterRecipesSelect,
+          Ordinary_Drink: true,
+        });
+        break;
+      case 'Other_Unknown':
+        setfilterSelect({
+          ...filterRecipesSelect,
+          Other_Unknown: true,
+        });
+        break;
+      default:
+        setfilterSelect({
+          ...filterRecipesSelect,
+          [target.name]: true,
+        });
+        break;
+      }
     }
   };
 
